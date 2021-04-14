9913577251e2475e68bff0f47bfcf411c91de2a8
robotlightsyou <robotlightsyou@gmail.com>
2021-04-14 12:30:09 -0700

    Create scope_demo.py

---
+++ scope_demo.py
@@ -0,0 +1,14 @@
+someword = "howdy"
+
+def main(data):
+    print_some_word(data)
+    data = 'test'
+    print_some_word(data)
+
+print(main)
+
+def print_some_word(word):
+    print(word)
+
+if __name__ == "__main__":
+    main(someword)
